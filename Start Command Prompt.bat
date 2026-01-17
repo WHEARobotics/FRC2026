dcf1012be5b905a9e313579048b50362e4042fda
Tam <tam.hoang@whea.net>
2026-01-16 16:05:02 -1000

    Start Command Pronpt update

--- Start Command Prompt.bat
+++ Start Command Prompt.bat
@@ -1,10 +1,8 @@
 :: Batch file to change to the proper folder and set up the environment.
 :: This file assumes that python poetry is on your computer and you have already
-:: run "poetry install" to create a virtual environment for the project.
-:: The FRC2025 repository should be on your desktop.
 @echo off
 echo ===================================================================================
-echo ==== Setup batch file for FRC2025 ====
+echo ==== Setup batch file for FRC2026 ====
 echo =
 echo    Usage:
 echo    To deploy, "robotpy deploy" when you are in that same folder.
@@ -14,8 +12,8 @@ echo    For more details about updating and deploying, see the docs folder.
 echo =
 echo ===================================================================================
 
-:: Change to the proper directory, the git repository for the FRC2025 code.
-cd %USERPROFILE%\Desktop\FRC2025
+:: Change to the proper directory, the git repository for the FRC2026 code.
+cd %USERPROFILE%\Desktop\FRC2026
 
 :: Activate the virtual environment set up for robotpy.
 call .\.venv\Scripts\activate.bat
